19b924863cf5a9471b388deb74db7402d988e125
Preethi Anbu <160149993+Preethianbu@users.noreply.github.com>
2024-02-21 21:54:44 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,314 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/Welcome/hangman.py =====================
+Welcome to 'Hangman', are you ready to play?
+(1) Yes, let's play!
+(2) No, get me outta here!
+-> 1
+I'm sorry, I didn't understand that.
+Welcome to 'Hangman', are you ready to play?
+(1) Yes, let's play!
+(2) No, get me outta here!
+-> yes
+Loading hangman...
+A crowd begins to gather, they can't wait to see some real justice.
+There's just one thing, you aren't a real criminal.
+No, no. You're the wrong time, wrong place type. You may think
+you're doomed, but it's not like that at all.
+Yes, yes. You've got a chance to live. All you've gotta do
+is guess the right word and you can live to see another day.
+But don't get too happy yet. If you make 6 wrong guesses,
+YOU'RE TOAST! VAMANOS!
+Guess a letter -> i
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     /
+            
+Progress: ??????
+Letters used: i
+Guess a letter -> a
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |
+            
+Progress: ??????
+Letters used: ia
+Guess a letter -> r
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|
+               |      |
+               |
+            
+Progress: ??????
+Letters used: iar
+Guess a letter -> e
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: ??????
+Letters used: iare
+Guess a letter -> p
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: p?????
+Letters used: iarep
+Guess a letter -> y
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: py????
+Letters used: iarepy
+Guess a letter -> t
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: pyt???
+Letters used: iarepyt
+Guess a letter -> h
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: pyth??
+Letters used: iarepyth
+Guess a letter -> o
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: pytho?
+Letters used: iarepytho
+Guess a letter -> n
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: python
+Letters used: iarepython
+Congratulations! You've guessed the word correctly.
+Would you like to play again? (yes/no): 
+=============================================================== RESTART: C:/Users/Welcome/hangman.py ==============================================================
+Welcome to 'Hangman', are you ready to play?
+(1) Yes, let's play!
+(2) No, get me outta here!
+-> yes
+Loading hangman...
+A crowd begins to gather, they can't wait to see some real justice.
+There's just one thing, you aren't a real criminal.
+No, no. You're the wrong time, wrong place type. You may think
+you're doomed, but it's not like that at all.
+Yes, yes. You've got a chance to live. All you've gotta do
+is guess the right word and you can live to see another day.
+But don't get too happy yet. If you make 6 wrong guesses,
+YOU'RE TOAST! VAMANOS!
+What is a widely-used general-purpose, object-oriented, high-level programming language? 
+Guess a letter -> p
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     / \
+            
+Progress: p????
+Letters used: p
+Guess a letter -> 
+=============================================================== RESTART: C:/Users/Welcome/hangman.py ==============================================================
+Welcome to 'Hangman', are you ready to play?
+(1) Yes, let's play!
+(2) No, get me outta here!
+-> yes
+Loading hangman...
+A crowd begins to gather, they can't wait to see some real justice.
+There's just one thing, you aren't a real criminal.
+No, no. You're the wrong time, wrong place type. You may think
+you're doomed, but it's not like that at all.
+Yes, yes. You've got a chance to live. All you've gotta do
+is guess the right word and you can live to see another day.
+But don't get too happy yet. If you make 6 wrong guesses,
+YOU'RE TOAST! VAMANOS!
+What is the data type in Python? 
+Guess a letter -> s
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     /
+            
+Progress: ?????
+Letters used: s
+Guess a letter -> f
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |
+            
+Progress: ?????
+Letters used: sf
+Guess a letter -> i
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |     \|
+               |      |
+               |
+            
+Progress: ?????
+Letters used: sfi
+Guess a letter -> b
+Things aren't looking so good, that guess was WRONG!
+Oh man, that crowd is getting happy, I thought you
+wanted to make them mad?
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: ?????
+Letters used: sfib
+Guess a letter -> p
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: ??p??
+Letters used: sfibp
+Guess a letter -> t
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: t?p??
+Letters used: sfibpt
+Guess a letter -> u
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: tup??
+Letters used: sfibptu
+Guess a letter -> l
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: tupl?
+Letters used: sfibptul
+Guess a letter -> e
+As it turns out, your guess was RIGHT!
+
+               --------
+               |      |
+               |      O
+               |      |
+               |      |
+               |
+            
+Progress: tuple
+Letters used: sfibptule
+Congratulations, you won! You've guessed the word correctly.
+Would you like to play again? (yes/no): no
+Goodbye!
